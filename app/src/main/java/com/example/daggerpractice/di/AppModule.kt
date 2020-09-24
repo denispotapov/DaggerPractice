b6bd5ab53b3f01c58aeae02ff5316a314a107039
Denis Potapov <fantom.r45@mail.ru>
2020-09-24 11:12:51 +0500

    Singleton Scope with Dagger 2

--- app/src/main/java/com/example/daggerpractice/di/AppModule.kt
+++ app/src/main/java/com/example/daggerpractice/di/AppModule.kt
@@ -9,11 +9,14 @@ import com.bumptech.glide.request.RequestOptions
 import com.example.daggerpractice.R
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class AppModule {
 
     companion object {
+
+        @Singleton
         @Provides
         fun provideRequestOptions(): RequestOptions {
             return RequestOptions
@@ -21,12 +24,14 @@ class AppModule {
                 .error(R.drawable.white_background)
         }
 
+        @Singleton
         @Provides
         fun provideGlideInstance(application: Application, requestOptions: RequestOptions): RequestManager {
             return Glide.with(application)
                 .setDefaultRequestOptions(requestOptions)
 
         }
+        @Singleton
         @Provides
         fun provideAppDrawable(application: Application): Drawable {
             return ContextCompat.getDrawable(application, R.drawable.logo)!!
