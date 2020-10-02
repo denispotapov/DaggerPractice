42e9a76160b8c1109cc1a6eabf2941e59e3e49f6
Denis Potapov <fantom.r45@mail.ru>
2020-10-02 21:26:40 +0500

    Toolbar Menu and Logout

--- app/src/main/java/com/example/daggerpractice/BaseActivity.kt
+++ app/src/main/java/com/example/daggerpractice/BaseActivity.kt
@@ -3,6 +3,9 @@ package com.example.daggerpractice
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.example.daggerpractice.models.User
 import com.example.daggerpractice.ui.auth.AuthActivity
--- app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.daggerpractice.ui.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.example.daggerpractice.BaseActivity
 import com.example.daggerpractice.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     private val TAG = "MainActivity"
@@ -11,7 +14,22 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.logout -> {
+                sessionManager.logOut()
+                Toast.makeText(this, "Сменить Activity", Toast.LENGTH_SHORT).show()
+                return true
+            } else -> super.onContextItemSelected(item)
+        }
     }
 }
